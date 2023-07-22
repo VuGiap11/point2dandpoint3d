2afca32646b273b866a4de25609e7bd7435bcf40
vu giap <vudinhgiap11@gmail.com>
2023-07-22 16:50:13 +0700

    first commit

---
+++ Point2D.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace point2Dand3D
+{
+    public class Point2D
+    {
+        private float x;
+        private float y;
+
+        public Point2D()
+        {
+
+        }
+        public Point2D(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+
+        public string Tostring(float x, float y)
+        {
+         return "point: "+ x + y;
+        }
+    }
+    public class Point3D : Point2D
+    {
+        private float Z;
+
+        public Point3D()
+        {
+
+        }
+        public Point3D(float x, float y, float z) : base(x,y)
+        {
+            this.Z = z;
+        }
+        public float z { get => z; set => z = value; }
+        public string Tostring(float x, float y, float z)
+        {
+           return "point: " + x + y + z;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace point2Dand3D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D p2 = new Point2D(1,2);
+            Console.WriteLine(p2);
+            Point3D p3 = new Point3D(1,2,3);
+            Console.WriteLine(p3);
+        }
+    }
+}
